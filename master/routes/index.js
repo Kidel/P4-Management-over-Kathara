d6af9ba6260d9c6af1d298213e261b35fd07fe1d
Veronica Iovinella <vero.neeja@gmail.com>
2017-07-18 19:17:05 +0200

    detect function done

--- master/routes/index.js
+++ master/routes/index.js
@@ -1,18 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var http = require("http");
+var fs = require('fs');
 
-var man_network = 'localhost';
-
-var options = {
-    host: man_network,
-    port: 3001,
-    path: '/',
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
-    }
-  }
+var man_network = ['localhost', 'wrong'];
 
 var ips = [];
 
@@ -23,37 +14,51 @@ router.get('/', function(req, res, next) {
 .get('/detect', function(req, res, next) {
   console.log('dentro get detect');
 
-  var get_req = http.request(options, function(res)
-    {
-        var output = '';
-        console.log(options.host + ':' + res.statusCode);
-        res.setEncoding('utf8');
-
-        res.on('data', function (chunk) {
-          console.log(chunk);
-            output += chunk;
-        });
-/*
-        res.on('end', function() {
-            var obj = JSON.parse(output);
-            onResult(res.statusCode, obj);
-        });*/
-
-        if (res.statusCode < 300){
-          ips.push(options.host);
+  /*Reset subscribers.txt and send get requests*/
+  fs.writeFile('subscribers.txt', '', function(){
+    console.log('subscribers reset');
+    man_network.forEach(function(ip) {
+      var options = {
+        host: ip,
+        port: 3001,
+        path: '/',
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
         }
+      }
+
+      var get_req = http.request(options, function(res)
+      {
+          console.log(options.host + ':' + res.statusCode);
+          res.setEncoding('utf8');
+
+          if (res.statusCode < 300){
+            ips.push(options.host);
+            fs.appendFile('subscribers.txt', options.host + '\n', function (err) {
+              if (err) throw err;
+              console.log('Saved!');
+            });
+            console.log(ips);
+          }
+      });
+
+      get_req.on('error', function(err) {
+        console.log(err.message);
+          //res.send('error: ' + err.message);
+      });
+
+      get_req.end();
     });
-
-    get_req.on('error', function(err) {
-      console.log(err.message);
-        //res.send('error: ' + err.message);
-    });
-
-    get_req.end();
-
-
+  });
 
     res.render('index', { title: 'Express' });
+})
+.get('/subscribe', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+})
+.get('/update', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
