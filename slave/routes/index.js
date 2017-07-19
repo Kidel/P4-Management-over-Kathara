896043c13a50830fff079d97d633ff04d7e90c9c
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2017-07-19 21:38:27 +0200

    attempt

--- slave/routes/index.js
+++ slave/routes/index.js
@@ -20,7 +20,6 @@ var update = function(req, res, next){
       var json_name = req.body.p4Name.replace('.p4', '.json');
 
       /*comandi*/
-      var child;
       fs.readFile('pid.txt', function (err, data) {
         if (err && typeof data != "undefined" && data.toString() != "") p4_pid = -1;
         else {
@@ -29,23 +28,15 @@ var update = function(req, res, next){
         }
         console.log("BM PID: " + p4_pid);
         var command1 = (p4_pid == -1 || p4_pid.length < 1)? "cd /p4c-bm" : "kill " + p4_pid + " && cd /p4c-bm";
-        child = exec(command1, function (error, stdout, stderr) {
-          console.log('stdout1: ' + stdout);
-          console.log('stderr1: ' + stderr);
-          if (error !== null) {
-            console.log('exec error: ' + error);
-          }
-          var compile;
-          compile = exec(
-            "p4c-bmv2 --json /P4-Management-over-Netkit/slave/" + json_name + " /P4-Management-over-Netkit/slave/" + req.body.p4Name 
-            + " && cd /PI && simple_switch -i 0@eth1 -i 1@eth2 /P4-Management-over-Netkit/slave/" + json_name + " </dev/null &>/dev/null &"
+        var child = exec(
+            command1 + " && p4c-bmv2 --json /P4-Management-over-Netkit/slave/" + json_name + " /P4-Management-over-Netkit/slave/" + req.body.p4Name 
+            + " && cd /PI && simple_switch -i 0@eth1 -i 1@eth2 /P4-Management-over-Netkit/slave/" + json_name + " &"
             , function (error, stdout, stderr) {
                 console.log('stdout2: ' + stdout);
                 console.log('stderr2: ' + stderr);
                 if (error !== null) {
                   console.log('exec error: ' + error);
                 }
-                
                 var getpid;
                 getpid = exec("ps axf | grep simple_switch | grep -v grep | awk '{print $1}'", function (error, stdout, stderr) {
                   console.log('stdout5: ' + stdout);
@@ -55,7 +46,6 @@ var update = function(req, res, next){
                       else console.log('Saved PID!');
                   });
                 });
-          });
         });
       });
     });
