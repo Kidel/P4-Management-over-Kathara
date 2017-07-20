51fa741057031773f0a2f2f9e5a975016164cb70
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2017-07-20 14:59:22 +0200

    minor fixes

--- lab/start_lab.bat
+++ lab/start_lab.bat
@@ -1,6 +1,7 @@
 @echo off
 
 CALL docker-compose up -d
+CALL docker exec -ti master sysctl net.ipv4.conf.all.rp_filter=0
 CALL docker network create netkit_nt_M
 CALL docker network connect netkit_nt_M master
 CALL docker exec -ti master ifconfig eth1 10.0.0.1/24 up
--- lab/start_lab.sh
+++ lab/start_lab.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker-compose up -d
+docker exec -ti master sysctl net.ipv4.conf.all.rp_filter=0
 docker network create netkit_${UID}_M
 docker connect netkit_${UID}_M master
 docker exec -ti master ifconfig eth1 10.0.0.1/24 up
